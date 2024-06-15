40c6df148d7ef043e513f91f3e74a7412af8c681
tpugh <tpugh@iastate.edu>
2024-06-15 14:43:23 -0700

    troubleshooting url_pattern regex - still troubleshooting

--- modules/trace_input.py
+++ modules/trace_input.py
@@ -5,7 +5,7 @@ def trace_input():
 
     # variables needed to begin a trace
     destination = ""
-    source_addr = default
+    #source_addr = default
     max_hops = 50   # OPTIONAL ADD ON user input for max_hops
     #timeout = 2    # OPTIONAL timeout if necessary
     
@@ -16,18 +16,22 @@ def trace_input():
     destination = input("Enter a destination (URL/IP): ")
 
     # define qualifications for destination input
-    url_pattern = r"((https?:\/\/[\w+-_]+.)|([\w+-_]+.)){1}([\wñÑ+-_]+.{1}[\w+]{2,10})"
+    #url_pattern = r"((https?:\/\/[\w+-_]+.)|([\w+-_]+.)){1}([\wñÑ+-_]+.{1}[\w+]{2,10})"
+    url_pattern = r"((((https?:\/\/\((w{3}.))?|((w{3}\.)?))([a-zA-Z]+[0,9])*\.)|([a-zA-Z]+[0,9])*\.)(([a-zA-Z]{2,3})+)((.{1})[a-zA-Z]{2})?"
     ip_pattern = r"(^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$)"
     
     # check validitiy of input destination
-    if destination is "":
+    if destination != "":
         if re.match(url_pattern, destination):
             print("Valid URL")
         elif re.match(ip_pattern, destination):
             print("Valid IP")
+        else:
+            print("Invalid input. Please enter a valid URL or IP address.")
     else:
         print("Invalid input. Please enter a valid URL or IP address.")
 
+trace = trace_input()
 
 # Will eventually bring source_address() in trace_input() 
 # because it makes more sense for them to be in the same code
