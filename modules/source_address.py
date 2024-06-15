e15a569aa0271423094bee88269bb0cc74a08326
tpugh <tpugh@iastate.edu>
2024-06-14 18:52:00 -0700

    began code for source_address validating the source IP address or choosing the default source addres (for the moment hard coded)

--- modules/source_address.py
+++ modules/source_address.py
@@ -1,1 +1,27 @@
+# This code will eventually be within trace_input not its own module
+# add the optional user input for a starting IP address 
+# this would allow the user to specify a specific starting point for the trace
+
+
+import re
+
+
 def source_address():
+
+    # variables needed
+    source_ip = input("Enter the source IP address (leave blank for default): ")
+    ip_pattern = r"(^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$)"  # pattern for IP address
+
+
+    if source_ip != "":
+        # validate the source IP address
+        if re.match(ip_pattern, source_ip):
+            return source_ip
+        else:
+            print("Invalid source IP address. Please enter a valid IP address.")
+    else:
+        # source ip is hardcoded for set up purposes
+        # FUTURE add code to retrieve default source address rather than hardcode
+        source_ip = "8.8.8.8"  # default source IP address
+        return source_ip
+    
