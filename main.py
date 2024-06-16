a640d79ce29f337dbcb440a54ee0deaf68e863e1
tpugh <tpugh@iastate.edu>
2024-06-16 12:22:15 -0700

    set up main.py for future function imports

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+# building the main code all together
 
+from validate_trace_ip import trace_import
+
+destination = input("Enter a destination (URL/IP): ")
+valid_dest = validate_trace_ip(destination)
