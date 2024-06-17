ea02d7c33e4edb0cd3efee8605ec527be2bf99a4
tpugh <tpugh@iastate.edu>
2024-06-16 18:43:47 -0700

    entered code for beginning a traceroute and setting up print() messages for identifying various points of code along the way

--- trace_route.py
+++ trace_route.py
@@ -10,13 +10,74 @@ import time
 from scapy.all import *
 import argparse
 
-
+from source_address import source_ip
+from dest_address import destination
 
 
 def trace_route(destination, source, max_hops=50, timeout=2):
+    
+    # defining variabls
+    timeout = 2
+    port = 33434
+    ttl = 1
+    ICMP = socket.getprotobyname('icmp')
+    UDP = socket.getprotobyname('udp')
+
+    # creating socket objects
+    icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, UDP)
+
+    timeout_struct = struct.pack('ll', timeout, 0)
+    icmp_socket.bind(("", port)) # binds the ICMP protocol to the port
+    icmp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, timeout_struct)
 
 
+    # defining the destination host
+    destination_ip = socket.gethostbyname(destination) # NOTE this is for a url NOT an IP
 
+    # for troubleshooting
+    print(f"Tracerouting...{destination}({destination_ip})")
+
+    while True:
+        # creating the UDP packet
+        udp_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+        udp_socket.sendto(b'', (destination_ip, port))
+        
+        # setting start time
+        start_time = time.time()
+        no_tries = 3
+        
+        # defining Bool
+        success = False
+        done = False
+
+        #
+        while no_tries > 0:
+            try:
+                # receiving the ICMP packet
+                packet, addr = icmp_socket.recvfrom(512)
+                success = True
+            except socket.error:
+                no_tries -= 1
+                continue
+            if addr[0] == destination_ip:
+                done = True
+                break
+        if success:
+            end_time = time.time()
+            try:
+                name = socket.gethostbyaddr(addr[0])[0]
+            except: pass
+            t = round((end_time - start_time) * 1000, 4)
+            print(f"TTL: {ttl} Addr: {name} ({addr[0]}) Time: {t}ms")
+        else:
+            print(f"TTL: {ttl} * * *")
+        
+        if done:
+            break
+        ttl += 1
+    
+    print("Traceroute completed.")
 
 
 if __name__ == "__main__":
