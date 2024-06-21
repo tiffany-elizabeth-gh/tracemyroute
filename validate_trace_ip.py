b9740100aebc5837b986372d19808617c4447361
tpugh <tpugh@iastate.edu>
2024-06-20 17:18:31 -0700

    TO BE DELETED :: this module will be deleted as it is redundant and unnecessary

--- validate_trace_ip.py
+++ validate_trace_ip.py
@@ -1,22 +1,16 @@
+# DELETE - unnecessary module
+
 import re
 
 
 def validate_trace_ip(destination):
-
-    # variables needed to begin a trace
-    destination = ""
-    #source_addr = default
-    max_hops = 50   # OPTIONAL ADD ON user input for max_hops
-    #timeout = 2    # OPTIONAL timeout if necessary
-    
-    # CH destination is given by argument, no need to input()!
 
     # define qualifications for destination input
     url_pattern = r"((https?\:\/\/)?((?:www\.))?)[a-zA-Z]+[0-9]*\.([a-zA-Z]{2,3})((?:\.[a-zA-Z]{2}))?"
     ip_pattern = r"(^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$)"
     
     # check validity of input destination
-    if destination != "":
+    if destination != None:
         if re.match(url_pattern, destination):
             return True, "Valid URL"
         elif re.match(ip_pattern, destination):
@@ -26,9 +20,6 @@ def validate_trace_ip(destination):
     else:
         return False, "Invalid input. Please enter a valid URL or IP address."
     
-
-
-
 
 
 if __name__ == "__main__":
