499c7936943bc3368bd222a8bc610298a9f30d63
tpugh <tpugh@iastate.edu>
2024-06-20 17:13:24 -0700

    cleaned up source_address to have appropriate True/False and call actions. renamed source_ip to source to keep things simple

--- source_address.py
+++ source_address.py
@@ -1,35 +1,34 @@
-# This code will eventually be within trace_input not its own module
-# add the optional user input for a starting IP address 
-# this would allow the user to specify a specific starting point for the trace
+''' source_address(source) is code to identify the source IP address for the traceroute.
+If the source is entered manually, the IP address is validated. If the manually entered source IP
+address is invalid, corresponding error message will be given.
+If the source is not entered manually, the default source address will be used.'''
 
 
 import re
 
 
-def source_address(source_ip):
+def source_address(source):
 
     # pattern to validate the source IP address
     # only necessary for when source IP address is manually entered
     ip_pattern = r"(^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$)"
 
 
-    if source_ip != "":
+    if source is not None:
         # validate the source IP address, if entered manually
-        if re.match(ip_pattern, source_ip):
-            return True, source_ip
+        if re.match(ip_pattern, source):
+            return True, f"Source IP is {source}"
         else:
-            return False, "Invalid source IP address. Please enter a valid IP address."
+            return False, f"Invalid source IP address {source}. Please enter a valid IP address or leave blank for default source IP."
     else:
         # source ip is hardcoded for set up purposes
         # FUTURE add code to retrieve default source address rather than hardcode
-        source_ip = "8.8.8.8"  # default source IP address
-        print(f"Default source: {source_ip} is being used.") # printing source_ip for error checking
-        return True, source_ip
-
-#source = source_address()
+        source = "8.8.8.8"  # default source IP address
+        return True, f"Source IP is {source}"
 
 if __name__ == "__main__":
-    source_address("192.168.0.1") # hardcode some to be validated source here
+    result, message = source_address("192.168.0.1")
+    print(message)
 
 
 # Future thoughts:
