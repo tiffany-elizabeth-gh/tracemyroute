8eeab0aa0b3aae187984f91fe380b13f75ac2bb0
tpugh <tpugh@iastate.edu>
2024-07-12 11:30:31 -0700

    changed if loop to a True/False call to work with flask_stream_HTML

--- source_address.py
+++ source_address.py
@@ -16,19 +16,20 @@ def source_address(source):
     ip_pattern = r"(^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$)"
 
 
-    if source is not None:
+    if source is True:
         # validate the source IP address, if entered manually
-        if re.match(ip_pattern, source):
-            return True, f"Source IP is {source}"
+        if re.match(ip_pattern, source) is True:
+            return f"Source IP is {source}"
         else:
             return False, f"Invalid source IP address {source}. Please enter a valid IP address or leave blank for default source IP."
     else:
         # code to retrieve user's IP address
         hostname = socket.gethostname()
         local_ip = socket.gethostbyname(hostname)
-        print(f"Your Computer IP Address is: {local_ip}")
+        source = local_ip
+        print(f"Your Computer IP Address is: {source}")
 
-        return True, f"Source IP is {local_ip}"
+        return f"Source IP is {source}"
 
 if __name__ == "__main__":
     result, message = source_address("192.168.0.1")
