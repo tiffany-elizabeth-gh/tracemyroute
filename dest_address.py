4054cebf40f88af3821dc1e5ba3eaa2a87fd1ae7
tpugh <tpugh@iastate.edu>
2024-07-21 13:58:00 -0700

    updated dest_address for error handling and identifying valid destinations

--- dest_address.py
+++ dest_address.py
@@ -7,45 +7,34 @@ corresponding error messages will be outputted'''
 
 import re
 import socket
+from urllib import parse 
 
 
 def dest_address(destination):
 
     # define qualifications for destination input
-    url_pattern = r"((https?\:\/\/)?((?:www\.))?)[a-zA-Z]+[0-9]*\.([a-zA-Z]{2,3})((?:\.[a-zA-Z]{2}))?"
+    #url_pattern = r"((https?\:\/\/)?((?:www\.))?)[a-zA-Z]+[0-9]*\.([a-zA-Z]{2,3})((?:\.[a-zA-Z]{2}))?"
     ip_pattern = r"(^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$)"
     
     # check validity of input destination
     if destination is not None:
-        if re.match(url_pattern, destination):
-            #convert url to IP
-            destination = socket.gethostbyname(destination)
-            #print(f"Destination IP is {destination}"")
-            return True, destination
-        # if destination is not a URL, check if it is an IP address
-        elif re.match(ip_pattern, destination):
-            #print(f"Destination IP is {destination}")
-            return True, destination
+        if re.match(ip_pattern, destination):
+             return True, destination
         else:
-            return False, "Invalid input. Please enter a valid URL or IP address."
-    # not a valid URL or IP address
+            try:
+                socket.gethostbyname(destination)
+            except socket.gaierror:
+                    return False, "Invalid URL. Try again."
+            else:
+                dest_split = parse.urlsplit(destination)
+                dest_ip = socket.gethostbyname(dest_split.netloc)
+
+                destination = dest_ip
+                return True, destination
+    # no entry made
     else:
-        return False, "Invalid input. Please enter a valid URL or IP address."
+        return False, "Must enter a URL to begin."
 
 if __name__ == "__main__":
-    result, message = dest_address("www.google.com")
+    result, message = dest_address("tate.org.uk")
     print(message)
-
-
-# call to action to work with manually inputted destination
-
-#if __name__ == "__main__":
-
-    #while True:
-        #destination = input("Enter a destination (URL/IP): ")
-        #result, err_str = validate_trace_ip(destination)
-
-        #if result == False:
-            #print(err_str, "try again!")
-        #else:
-            #break
